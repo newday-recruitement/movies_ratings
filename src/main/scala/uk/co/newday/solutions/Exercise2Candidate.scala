5037fbd546b2a0f2db8ff5f5c0a23db472848f8f
nikhita93 <102796449+nikhita93@users.noreply.github.com>
2022-04-01 10:18:54 +0100

    Update Exercise2Candidate.scala
    nikhita's solution

--- src/main/scala/uk/co/newday/solutions/Exercise2Candidate.scala
+++ src/main/scala/uk/co/newday/solutions/Exercise2Candidate.scala
@@ -5,7 +5,6 @@ import org.apache.spark.sql.DataFrame
 object Exercise2Candidate {
 
   def execute(movies: DataFrame, ratings: DataFrame): (DataFrame) = {
-    //With two dataframe apply the join as specified in the exercise.
-    null
+    val joined_df=ratings_df.join(movies_df,"MovieID")
   }
 }
